80822592a7d107a1b6d9cb028f6713b4ff79c368
Jonathan Ghee <jonathan.p.ghee@gmail.com>
2023-12-02 10:13:10 -0600

    skipping day 1 since I did it in Go. I forgot how to use IntelliJ + Git. hopefully this worked

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
+/*
+I did this is in Go first, and then watched the Kotlin guys do it(Jetbrains is sponsoring the event btw!)
+kotlin is a way nicer way to solving things because of the libraries available.
+Going to do day 2 in kotlin first and see how it goes
+ */
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+            return calibrationValue(input[0])
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+   // part1(input).println()
+   // part2(input).println()
+}
+
+fun calibrationValue(row:String): Int =
+    "${row.first {it.isDigit()}}${row.last{it.isDigit()}}".toInt()
+
+fun String.possibleWordsAt(startingAt: Int): List<String> = (3..5).map {
+    len -> substring(startingAt, (startingAt+len).coerceAtMost(length))
 }
---
+++ src/Day02.kt
@@ -0,0 +1,6 @@
+fun main(){
+    fun part1(){
+    }
+    fun part2(){
+    }
+}
